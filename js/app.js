07d5b89c132012178125a7aa66d05e742b26de93
Joakim Wassberg <joakim@arthead.se>
2022-09-27 14:02:37 +0200

    Solution 1 done

--- js/app.js
+++ js/app.js
@@ -12,6 +12,12 @@ selections.forEach((selection) => {
         const cInput = cOptions[Math.floor(Math.random() * 3)];
 
         updateMoveImgs(pInput, cInput);
+        compareInputs(pInput, cInput);
+        updateScore();
+        if(checkWin()) {
+            pScore = cScore = 0;
+            updateScore();
+        }
     });
 });
 
@@ -20,3 +26,59 @@ function updateMoveImgs(pInput, cInput) {
     document.getElementById("c-move").src = `./img/${cInput}.svg`;
 }
 
+function compareInputs(pInput, cInput) {
+    if(pInput === cInput) { 
+        document.getElementById("result-text").innerHTML = "It is a tie!";
+        return;
+    }
+
+    if(pInput === "rock") {
+        if(cInput == "scissors") {
+            document.getElementById("result-text").innerHTML = "You win!";
+            pScore++;
+        }
+        else {
+            document.getElementById("result-text").innerHTML = "Computer win!";
+            cScore++;
+        }
+    }
+
+    else if(pInput === "paper") {
+        if(cInput == "rock") {
+            document.getElementById("result-text").innerHTML = "You win!";
+            pScore++;
+        }
+        else {
+            document.getElementById("result-text").innerHTML = "Computer win!";
+            cScore++;
+        }
+    }
+    // pInput == "scissors"
+    else {
+        if(cInput == "paper") {
+            document.getElementById("result-text").innerHTML = "You win!";
+            pScore++;
+        }
+        else {
+            document.getElementById("result-text").innerHTML = "Computer win!";
+            cScore++;
+        }
+    }
+}
+
+function updateScore() {
+    document.getElementById("p-score").innerHTML = pScore;
+    document.getElementById("c-score").innerHTML = cScore;
+}
+
+
+function checkWin() {
+    if(pScore === 5 || cScore === 5) {
+        const winner = pScore === 5
+            ? "You win the game! Congratulations!"
+            : "Computer wins the game. Better luck next time";
+        document.getElementById("result-text").innerHTML = winner;
+        return true;
+    }
+    return false;
+}
